501887b721f35219b40dad0f5719843c39e8213e
namclu <namlu1205@gmail.com>
2017-01-21 16:41:13 +1100

    2 Add initial implementation of TourItem, which contains two String objects; a title and description

---
+++ app/src/main/java/com/example/namlu/sydneywalkabout/TourItem.java
@@ -0,0 +1,46 @@
+package com.example.namlu.sydneywalkabout;
+
+/**
+ * Created by namlu on 21-Jan-17.
+ * <p>
+ * TourItem represents a single tour item that the user wants to visit.
+ */
+
+public class TourItem {
+
+    // String resource ID for the TourItem title
+    private String mTourItemTitle;
+
+    // String resource ID for the TourItem description
+    private String mTourItemDescription;
+
+    /*
+     * Create a new TourItem object.
+     *
+     * @param mTourItemTitle
+     */
+    public TourItem(String tourItemTitle, String tourItemDescription){
+        mTourItemTitle = tourItemTitle;
+        mTourItemDescription = tourItemDescription;
+    }
+
+    /*
+     * Getter and setter methods for TourItem
+     */
+
+    public String getTourItemTitle() {
+        return mTourItemTitle;
+    }
+
+    public void setTourItemTitle(String tourItemTitle) {
+        mTourItemTitle = tourItemTitle;
+    }
+
+    public String getTourItemDescription() {
+        return mTourItemDescription;
+    }
+
+    public void setTourItemDescription(String tourItemDescription) {
+        mTourItemDescription = tourItemDescription;
+    }
+}
