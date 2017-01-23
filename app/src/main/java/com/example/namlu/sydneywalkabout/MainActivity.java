9b4c91eab326e40b6779e43967db91d23484c686
namclu <namlu1205@gmail.com>
2017-01-23 15:10:21 +1100

    7 Implement Butterknife library and update MainActivity to use. Error: java.lang.NullPointerException

--- app/src/main/java/com/example/namlu/sydneywalkabout/MainActivity.java
+++ app/src/main/java/com/example/namlu/sydneywalkabout/MainActivity.java
@@ -5,22 +5,28 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     // Declare instance variables
-    private TabLayout mTabLayout;
-    private ViewPager mViewPager;
+    @BindView(R.id.tabs_main) TabLayout mTabLayout;
+    @BindView(R.id.viewpager_main) ViewPager mViewPager;
+    //private TabLayout mTabLayout;
+    //private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         // Find the view pager that will allow the user to swipe between fragments
-        mViewPager = (ViewPager) findViewById(R.id.viewpager_main);
+        //mViewPager = (ViewPager) findViewById(R.id.viewpager_main);
         setupViewPager(mViewPager);
 
-        mTabLayout = (TabLayout) findViewById(R.id.tabs_main);
+        //mTabLayout = (TabLayout) findViewById(R.id.tabs_main);
         mTabLayout.setupWithViewPager(mViewPager);
     }
 
